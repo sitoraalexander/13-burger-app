c7f66f28e317f0c0da375d017a4937ea52380230
Sitora Alexander <sitoraalexander@gmail.com>
2021-01-16 21:20:44 -0500

    Model setup

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  create: function(name, cb) {
+    orm.create("burgers", [
+      "burger_name", "devour"
+    ], [
+      name, false
+    ], cb);
+  },
+  update: function(id, cb) {
+    var condition = "id=" + id;
+    orm.update("burgers", {
+      devour: true
+    }, condition, cb);
+  }
+};
+
+module.exports = burger;
